4b84bff215e39c5b0ef4a975f6014d2c5cac6ecb
carloszb27 <carloszbd.27@gmail.com>
2025-05-28 16:17:12 -0500

    Se agregan los perfiles de la aplicacion

--- src/test/java/com/proyectos/ProyectoEcommerce/ProyectoEcommercev2/ProyectoEcommercev2ApplicationTests.java
+++ src/test/java/com/proyectos/ProyectoEcommerce/ProyectoEcommercev2/ProyectoEcommercev2ApplicationTests.java
@@ -2,8 +2,10 @@ package com.proyectos.ProyectoEcommerce.ProyectoEcommercev2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProyectoEcommercev2ApplicationTests {
 
 	@Test
